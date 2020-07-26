c071082d684ad6a9a86ed254d54f9015b28209f4
KevinJoseph <kevramos95@gmail.com>
2020-07-26 06:38:49 -0500

    v1.0.0.1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+
+/*if(process.env.NODE_ENV === 'development'){
 import dotenv from 'dotenv';
-if(process.env.NODE_ENV === 'development'){
 dotenv.config();
-}
+}*/
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
+import dotenv from 'dotenv';
 
 const app = express();
 
 //Connect to Database
 const mongoose = require('mongoose');
-const uri = process.env.DB_URI;
+const uri = 'mongodb+srv://vekjs:p131471483@cluster0-yzzb4.mongodb.net/test?retryWrites=true&w=majority';
 const options = {useNewUrlParser: true, useUnifiedTopology:true};
 
 mongoose.connect(uri, options)
@@ -35,7 +37,7 @@ app.use('/request', require('./routes/request'));
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
 app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../','public')));
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
